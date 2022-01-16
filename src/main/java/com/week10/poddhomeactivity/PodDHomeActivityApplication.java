587a47d6c0699707d188383b519d4323e3b1e334
Amy-Oji <amyojiakor@gmail.com>
2022-01-16 20:49:04 +0100

    initial commit

---
+++ src/main/java/com/week10/poddhomeactivity/PodDHomeActivityApplication.java
@@ -0,0 +1,13 @@
+package com.week10.poddhomeactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodDHomeActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PodDHomeActivityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/week10/poddhomeactivity/PodDHomeActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week10.poddhomeactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodDHomeActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
